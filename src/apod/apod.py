ac78c90a82eec392a5296ec7edc8217b1a626d18
Peter Grivalsky <pp.grivalsky@gmail.com>
2026-01-26 21:51:27 +0100

    refactoring: split APOD logic into functions

--- src/apod/apod.py
+++ src/apod/apod.py
@@ -4,33 +4,47 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # GET DATA from API 
-NASA_API_KEY = os.getenv("NASA_API_KEY")
-source = requests.get("https://api.nasa.gov/planetary/apod", params={"api_key" : NASA_API_KEY}, timeout=10)
-data = source.json()
-print(data)
+def get_data():
+    NASA_API_KEY = os.getenv("NASA_API_KEY")
+    if not NASA_API_KEY:
+        raise RuntimeError("API KEY not set!")
+    source = requests.get("https://api.nasa.gov/planetary/apod", params={"api_key" : NASA_API_KEY}, timeout=10)
+    source.raise_for_status()
+    data = source.json()
+    return data
 
-# CREATE IMG 
-title = data["title"]
-place_img = f"/Users/Fidel/Desktop/{title}.jpg"
-img_url = data["hdurl"]
-img_data = requests.get(img_url, timeout=10).content
-with open(place_img, "wb") as f:
-    f.write(img_data)
+# CREATE .IMG 
+def save_image(data):
+    title = data["title"]
+    place_img = f"/Users/Fidel/Desktop/{title}.jpg"
+    img_url = data["hdurl"]
+    img_data = requests.get(img_url, timeout=10).content
+    with open(place_img, "wb") as f:
+        f.write(img_data)
 
 
-# CREATE TXT
-title = data["title"]
-place_txt = f"/Users/Fidel/Desktop/{title}.txt"
-with open(place_txt, "w") as f:
-    json.dump(data, f, indent=2)
+# CREATE .TXT
+def save_meta(data):
+    title = data["title"]
+    place_txt = f"/Users/Fidel/Desktop/{title}.txt"
+    with open(place_txt, "w") as f:
+        json.dump(data, f, indent=2)
 
 
 
 # MAIN WRAPPER
-# def main():
+def main():
+    try: 
+        data = get_data()
+        save_image(data)
+        save_meta(data)
+    except requests.RequestException as e:
+        print(f"Network error: {e}")
+    except KeyError as e: 
+        print(f"Key Error: {e}")
+    except Exception as e:
+        print(f"Problem: {e}")
 
 
-
-
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
