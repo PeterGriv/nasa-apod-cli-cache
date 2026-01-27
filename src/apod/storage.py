afb15c9839218deaed1ca48fd11f6980b8fc1540
Peter Grivalsky <pp.grivalsky@gmail.com>
2026-01-27 09:51:12 +0100

    refactor: replacing absolute path with relative output

--- src/apod/storage.py
+++ src/apod/storage.py
@@ -1,11 +1,14 @@
 import requests
 import json
+import os
 
+OUTPUT_DIR = "output"
+os.makedirs(OUTPUT_DIR, exist_ok = True)
 
 # CREATE .IMG 
 def save_image(data):
     title = data["title"]
-    place_img = f"/Users/Fidel/Desktop/{title}.jpg"
+    place_img = f"{OUTPUT_DIR}/{title}.jpg"
     img_url = data["hdurl"]
     img_data = requests.get(img_url, timeout=10).content
     with open(place_img, "wb") as f:
@@ -15,6 +18,6 @@ def save_image(data):
 # CREATE .TXT
 def save_meta(data):
     title = data["title"]
-    place_txt = f"/Users/Fidel/Desktop/{title}.txt"
+    place_txt = f"{OUTPUT_DIR}/{title}.txt"
     with open(place_txt, "w") as f:
         json.dump(data, f, indent=2)
